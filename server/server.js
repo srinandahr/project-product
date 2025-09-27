f9f6cc38aac28e4d6b4dd6a16dd4e4f10edec341
srinanda <hsrinanda@gmail.com>
2025-09-27 23:37:20 +0530

    Fix Sequelize SSL

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const DSA = require('./models/DSA');
 const Project = require('./models/Project');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
