3dc78ff5b67fbce55f3f0cc02bb26e2cc4021b06
Srinanda <94759528+srinandahr@users.noreply.github.com>
2026-01-17 23:32:01 +0530

    Import CSS file in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './components/Navbar'
+import "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
