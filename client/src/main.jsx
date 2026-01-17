94034f1416cf240dc555f99eecb29c968fa2cbb6
srinandahr <hsrinanda@gmail.com>
2026-01-17 23:22:54 +0530

    Moved tailwind dependencies to client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Navbar from './components/Navbar'
 
 createRoot(document.getElementById('root')).render(
